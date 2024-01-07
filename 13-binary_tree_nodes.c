64a5d908fff600ad24a9789a621d1da1dd5bc8a0
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-07 16:19:59 -0300

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    if (tree)
-    {
-        if (tree->left || tree->right)
-            nodes = 1;
+	if (tree)
+	{
+		if (tree->left || tree->right)
+			nodes = 1;
 
-        nodes += binary_tree_nodes(tree->left);
-        nodes += binary_tree_nodes(tree->right);
-    }
-    return (nodes);
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
