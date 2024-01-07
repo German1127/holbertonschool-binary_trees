987c2b962ec256f702d9f7a1b80308cba827267a
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-07 17:42:19 -0300

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
-
 /**
-* binary_tree_inorder - goes through a binary tree using in-order transversal
-* @tree: pointer to root node of tree to transverse
-* @func: pointer to function
-*/
+ * binary_tree_inorder - goes through a binary tree using in-order transversal
+ * @tree: pointer to root node of tree to transverse
+ * @func: pointer to function
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
