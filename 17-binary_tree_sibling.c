33cf787ab9d0ac9392e3ecb8035e4b23ffa677c4
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-07 20:36:26 -0300

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	binary_tree_t *parent = node->parent;
 if (parent->left == node && parent->right != NULL)
+return (parent->right);
+
+else if (parent->right == node && parent->left != NULL)
 return (parent->left);
 
 return (NULL);
