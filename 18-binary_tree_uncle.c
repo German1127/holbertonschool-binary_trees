984b7fb3929b4fb7a5e369e51ce7dea4874a0414
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-07 21:06:05 -0300

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,14 +7,13 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (!node || !node->parent || !node->parent->parent)
 	return (NULL);
 
-if (node->parent->parent-left != NULL && node->parent->parent->left != node->parent)
+	if (node->parent == node->parent->parent->left)
+	return (node->parent->parent->right);
+
+else
 return (node->parent->parent->left);
 
-else if (node->parent->parent->right != NULL && node->parent->parent->right != node->parent)
-return (node->parent->parent->right);
-
-return (NULL);
 }
